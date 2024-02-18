c4d9b88886f3b54b8a0da696a1ff4991a9a77920
khushboo204 <khushboorathod20@gmail.com>
2024-02-18 15:37:40 -0500

    Initial classes creation

--- Gem Hunters/Program.cs
+++ Gem Hunters/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Position
+{
+    public int x;
+    public int y;
+
+    public Position(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+class player
+{
+    public string Name;
+    Position position;
+    public int Gemcount;
+ 
+    public void Move(char direction)
+    {
+        switch (direction)
+        {
+            case 'U':
+                break;
+            case 'D':
+                break;
+            case 'L':
+                break;
+            case 'R':
+                break;
+        }
+    }
+}
+
